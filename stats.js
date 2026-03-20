5cd4819aff48daba45979bd07e6f3bc58221fc6c
foodrecoveryalliance <foodrecoveryalliance@gmail.com>
2026-03-20 15:12:34 -0700

    Add files via upload

--- stats.js
+++ stats.js
@@ -1,13 +1,14 @@
 const FRA_STATS = {
-    pounds:        { display: '8,100',   count: 7200  },
-    dollars:       { display: '$29000', count: 25000 },
-    volunteers:    { display: '200',     count: 200    },
+    pounds:        { display: '8,156',   count: 8156  },
+    dollars:       { display: '$29,252', count: 29252 },
+    volunteers:    { display: '140',     count: 140    },
     organizations: { display: '75',     count: 75    },
     leaders:    { display: '16',     count: 16    },
     schools:    { display: '3',     count: 3    },
-    events:    { display: '20',     count: 10  },
+    events:    { display: '20',     count: 20  },
     reached:    { display: '75', count: 75  },
-    percent:    { display: '10', count: 10  }
+    percent:    { display: '10', count: 10  },
+    goal:    { display: '80,000', count: 80000  }
 };
 
 document.querySelectorAll('[data-stat]').forEach(el => {
